431796c5d1e5c7092db08d5495ee0e5aa54bb66f
RitheshNagaraj <rithesh.nagaraj@thantrick.in>
2024-07-28 21:51:56 +0530

    Cunsuming custom object & Java Based configuration

--- src/main/java/com/thantrick/SpringBoot_Kafka_Consumer/service/kafkaConsumerService.java
+++ src/main/java/com/thantrick/SpringBoot_Kafka_Consumer/service/kafkaConsumerService.java
@@ -1,5 +1,6 @@
 package com.thantrick.SpringBoot_Kafka_Consumer.service;
 
+import com.thantrick.SpringBoot_Kafka_Consumer.dto.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,23 +11,34 @@ public class kafkaConsumerService {
 
     Logger logger = LoggerFactory.getLogger(kafkaConsumerService.class);
 
-    @KafkaListener(topics = "demoTopic3-from-Config", groupId = "demoGroup2")
+    @KafkaListener(topics = "msgTopic", groupId = "msg-consumer-group")
     public void consumeMessage1(String msg){
         logger.info("Consumer1 consumed message : {}", msg);
     }
 
-    @KafkaListener(topics = "demoTopic3-from-Config", groupId = "demoGroup2")
+    /*@KafkaListener(topics = "msgTopic", groupId = "msg-consumer-group")
     public void consumeMessage2(String msg){
         logger.info("Consumer2 consumed message : {}", msg);
-    }
+    }*/
 
-    @KafkaListener(topics = "demoTopic3-from-Config", groupId = "demoGroup2")
+    /*@KafkaListener(topics = "msgTopic", groupId = "msg-consumer-group")
     public void consumeMessage3(String msg){
         logger.info("Consumer3 consumed message : {}", msg);
     }
 
-    @KafkaListener(topics = "demoTopic3-from-Config", groupId = "demoGroup2")
+    @KafkaListener(topics = "msgTopic", groupId = "msg-consumer-group")
     public void consumeMessage4(String msg){
         logger.info("Consumer4 consumed message : {}", msg);
+    }*/
+
+    @KafkaListener(topics = "empTopic....", groupId = "emp-consumer-group")
+    public void consumeEmployee1(Employee emp){
+        logger.info("Emp consumer1 consume the employee events {} ", emp.toString());
     }
+
+    /*@KafkaListener(topics = "empTopic", groupId = "emp-consumer-group")
+    public void consumeEmployee2(Employee emp){
+        logger.info("Emp consumer2 consume the employee events {} ", emp.toString());
+    }*/
+
 }
